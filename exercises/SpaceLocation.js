37b90ee17ca4245e9ec8b5b4f64592fc48a022c0
Forrest <forrestburrelsman@gmail.com>
2020-03-24 17:51:42 -0500

    changes to parts1-5 and SpaceLocation

---
+++ exercises/SpaceLocation.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var SpaceLocation = /** @class */ (function () {
+    function SpaceLocation(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+    SpaceLocation.prototype.printDaysToLocation = function (location) {
+        console.log(this.name + " would take " + this.getDaysToLocation(location.kilometersAway) + " days to get to " + location.name + ".");
+    };
+    SpaceLocation.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesAway = kilometersAway * this.milesPerKilometer;
+        var hoursToLocation = milesAway / this.speedMph;
+        var daysToLocation = hoursToLocation / 24;
+        return daysToLocation;
+    };
+    return SpaceLocation;
+}());
+exports.SpaceLocation = SpaceLocation;
--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,20 @@
 // Paste in the provided code here:
+import { Spacecraft } from './parts1-5';
+export class SpaceLocation {
+    kilometersAway: number;
+    name: string;
+ 
+    constructor(name: string, kilometersAway: number) {
+       this.name = name;
+       this.kilometersAway = kilometersAway;
+    }
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+     }
+     getDaysToLocation(this: any, kilometersAway: number) : number {
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hoursToLocation: number = milesAway / this.speedMph;
+        let daysToLocation: number = hoursToLocation / 24
+        return daysToLocation
+    }
+ }
